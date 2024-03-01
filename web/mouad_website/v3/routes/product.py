1fbb9760dccf59277397f9981b97789fe94ec4ea
kouirsfatima <fatimaberline535@gmail.com>
2024-03-01 10:27:26 +0100

    1

--- web/mouad_website/v3/routes/product.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-from routes.__init__ import view_blueprint
-from flask import render_template
-
-@view_blueprint.route('/product/<int:id>', strict_slashes=False)
-def prduct(id):
-    return render_template('product.html')
--- web/mouad_website/v4/routes/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-view_blueprint = Blueprint('view_blueprint', __name__)
-
-from routes.product import *
